108cf1c9dba9220a1837d10ae49c8978589bafa9
Samantha Wong <smiwong920@gmail.com>
2019-01-30 01:51:14 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  "#{value}"
 end
 
-def argue
-  
+def argue(arguement)
+  "#{arguement}"
 end
 
-def greeting
-
+def greeting(thing1, thing2)
+ "#{thing1}, #{thing2}"
 end
 
-def return_a_value
-
+def return_a_value(phrase ="Nice")
+"#{phrase}"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase ="expert")
+ "#{phrase}"
 end
 
-def pizza_party
-
+def pizza_party(topping ="cheese")
+ "#{topping}"
 end
